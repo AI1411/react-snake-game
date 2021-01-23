5d58bd155c84104481acb9e2f5a9e1ebb07f170f
AI1411 <goethe0719@gmail.com>
2021-01-23 17:17:34 +0900

    snakeが動くように実装

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import Field from "./components/Field";
+import Navigation from "./components/Navigation";
+import ManipulationButton from "./components/ManipulationPanel";
+import Button from "./components/Button";
+import {initFields} from "./utils";
+
+const initialPosition = {x: 17, y: 17}
+const initFieldsValues = initFields(35, initialPosition);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [fields, setFields] = useState(initFieldsValues);
+    const [position, setPosition] = useState();
+
+    useEffect(() => {
+        setPosition(initialPosition);
+    }, []);
+
+    const goUp = () => {
+        const {x, y} = position;
+        const nextY = Math.max(y - 1, 0);
+        fields[y][x] = '';
+        fields[nextY][x] = 'snake';
+        setPosition({x, y: nextY});
+        setFields(fields);
+    }
+    return (
+        <div className="App">
+            <header className="header">
+                <div className="title-container">
+                    <h1 className="title">Snake Game</h1>
+                </div>
+                <Navigation/>
+            </header>
+            <main className="main">
+                <Field fields={fields}/>
+            </main>
+            <div style={{padding: '16px'}}>
+                <button onClick={goUp}>進む</button>
+            </div>
+            <Button/>
+            <ManipulationButton/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ManipulationPanel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ManipulationButton = () => {
+    return (
+        <div className="manipulation-panel">
+            <button>←</button>
+            <button>↑</button>
+            <button>↓</button>
+            <button>→</button>
+        </div>
+    );
+};
+
+export default ManipulationButton;
